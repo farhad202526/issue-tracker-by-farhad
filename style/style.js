30318b032f68b289d22e3b9afc9feadc4a7b83bc
farhad202526 <fa99.official@gmail.com>
2026-03-07 16:11:34 +0600

    a set up login page

---
+++ style/style.js
@@ -0,0 +1,25 @@
+
+
+// loging sectoin
+
+const userName = document.getElementById("username");
+const password = document.getElementById("password");
+const loginBtn = document.getElementById("login-btn");
+
+// funtion run for login section
+loginBtn.addEventListener("click", function () {
+    const usernameValue = userName.value.trim().toLowerCase(); 
+    const passwordValue = password.value.trim().toLowerCase(); 
+
+
+    if (usernameValue === "admin" && passwordValue === "admin1234") {
+        /* alert("Login successful!"); */
+        window.location.href = "home.html";
+    } else {
+        alert("Invalid username or password. Please try again.");
+    }
+})
+//  end loging section
+
+
+
